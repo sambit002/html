0b5fd1b4aa0964eb11f2778ee4952c2d908f88f9
sambit002 <sambit5150@gmail.com>
2023-08-03 22:07:33 +0530

    matrix rain effect

---
+++ canvas/trials2/gravity.js
@@ -0,0 +1,10 @@
+/**
+ * this is where we shall be coding gravity effect
+ */
+const canvas = document.getElementById("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+//creating a context
+let c = canvas.getContext('2d');
+
+
---
+++ canvas/trials2/matrix/matrix.js
@@ -0,0 +1,40 @@
+const canvas = document.getElementById("matrix");
+const c = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
+const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const nums = '0123456789';
+
+const alphabet = katakana + latin + nums;
+const fontSize = 16;
+
+const columns = canvas.width/fontSize;
+
+const rainDrops = [];
+
+for (let i=0; i<columns; i++) {
+    rainDrops[i] = 1;
+}
+
+const draw = () => {
+    c.fillStyle = 'rgba(0,0,0,0.05)';
+    c.fillRect(0, 0, canvas.width, canvas.height);
+    c.fillStyle = '#0F0';
+    c.font = fontSize + 'px monospace';
+
+    for (let i=0; i<rainDrops.length; i++) {
+        const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+        c.fillText(text, i*fontSize, rainDrops[i]*fontSize);
+
+        if(rainDrops[i]*fontSize > canvas.height && Math.random() > 0.975) {
+            rainDrops[i] = 0;
+        }
+        rainDrops[i]++;
+    }
+};
+
+setInterval(draw, 30);
+
